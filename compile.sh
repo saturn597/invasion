79d0cf7b1082303945093a80ac252323819e23dd
saturn597 <saturn5971@gmail.com>
2018-10-14 14:10:51 -0400

    Initial commit
    Produces a ROM file containing the iNES header.

---
+++ compile.sh
@@ -0,0 +1,2 @@
+ca65 invasion.s -o invasion.o
+ld65 -o invasion.nes -C config.cfg invasion.o
---
+++ invasion.s
@@ -0,0 +1,19 @@
+; Invasion - a simple NES game.
+
+
+; iNES header (needed for emulators)
+; see https://wiki.nesdev.com/w/index.php/INES for header format
+
+.segment "HEADER"
+
+.byte 'N', 'E', 'S', $1A
+.byte $02  ; tell emulator to expect 2 16 KB chunks of code for game
+.byte $01  ; tell emulator to expect 1 8 KB chunk of CHR/graphics data
+.byte %00000001  ; vertical mirroring/no battery backed memory/no trainer/NROM
+.byte $00  ; NROM/not NES 2.0/not playchoice-10/not a vs game
+.byte $00  ; assumes 8 KB of PRG RAM
+.byte $00  ; rarely used TV system info and reserved bits
+.byte $00  ; not part of official spec
+.byte $00, $00, $00, $00, $00  ; zero-filled, pads header to 16 bytes
+
+
