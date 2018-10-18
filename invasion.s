bac7b9d3e5172f3da62adfe3443df302684ee660
saturn597 <saturn5971@gmail.com>
2018-10-18 00:23:53 -0400

    Added reset code
    Shows a greenish screen.

--- invasion.s
+++ invasion.s
@@ -1,8 +1,10 @@
 ; Invasion - a simple NES game.
 
 
+;
 ; iNES header (needed for emulators)
 ; see https://wiki.nesdev.com/w/index.php/INES for header format
+;
 
 .segment "HEADER"
 
@@ -17,3 +19,94 @@
 .byte $00, $00, $00, $00, $00  ; zero-filled, pads header to 16 bytes
 
 
+;
+; Vectors - addresses for nmi, reset, irq.
+;
+
+.segment "VECTORS"
+
+.word nmi
+.word reset
+.word irq
+
+
+;
+; Reserve memory for OAM (object attribute memory/sprite data).
+;
+
+.segment "OAM"
+oam: .res 256
+
+
+;
+; Code segment - the main code for the game.
+;
+
+.segment "CODE"
+
+nmi:
+  rti
+
+
+reset:
+  sei              ; disable interrupts
+  cld              ; disable decimal mode
+  ldx #%01000000
+  stx $4017        ; Stop certain APU interrupts.
+  ldx #$ff         ; Stack goes $01ff to $0100, starting at $01ff.
+  txs              ; S register is stack pointer.
+  inx              ; X is now 0.
+  stx $2000        ; Disable nmi.
+  stx $2001        ; Disable rendering.
+  stx $4010        ; More APU settings.
+
+  ; We have to wait for vblank to ensure PPU is ready.
+  ; Loop until vblank flag (bit 7 of PPU status) is set. (BIT copies bit 7
+  ; at the location given to the negative flag, so can test result with bpl).
+  ; Also, vblank bit could be set at reset, so have to clear it first by
+  ; reading the PPU status once before starting.
+  bit $2002
+  :
+    bit $2002
+    bpl :-
+
+  ; Now set ram to known state.
+  ; (All zeroes, except place sprites off screen).
+  lda #$00
+  ldx #$00
+  :
+    sta $0000, x
+    sta $0100, x
+    sta $0200, x
+    sta $0300, x
+    sta $0400, x
+    sta $0500, x
+    sta $0600, x
+    sta $0700, x
+    inx
+    bne :-
+
+  lda #$ff
+  ldx #$00
+  :
+    sta oam, x
+    inx
+    inx
+    inx
+    inx
+    bne :-
+
+  ; wait for one more vblank
+  :
+    bit $2002
+    bpl :-
+
+  lda #%01000000  ; emphasize greens
+  sta $2001
+
+  :
+    jmp :-
+
+
+irq:
+  rti
