b83a753f9869eac7a19e0ab679b91bb23c1291c5
saturn597 <saturn5971@gmail.com>
2018-10-23 09:04:16 -0400

    Use controller to move sprite around

--- invasion.s
+++ invasion.s
@@ -3,6 +3,16 @@
 
 SPRITECOUNT = 1
 
+PAD_A      = $80
+PAD_B      = $40
+PAD_SELECT = $20
+PAD_START  = $10
+PAD_U      = $08
+PAD_D      = $04
+PAD_L      = $02
+PAD_R      = $01
+
+
 ;
 ; iNES header (needed for emulators)
 ; see https://wiki.nesdev.com/w/index.php/INES for header format
@@ -48,16 +58,122 @@ oam: .res 256
 
 
 ;
+; Zero page memory.
+;
+
+.segment "ZEROPAGE"
+gamepad:       .res 1
+gamepad_tmp:   .res 1
+
+
+;
 ; Code segment - the main code for the game.
 ;
 
 .segment "CODE"
 
+read_gamepad:
+  ;; Gamepad process: write 1 and 0 to $4016 to latch current button positions.
+  ;; Then read from $4016 to get the state of the "A" button. Read from $4016
+  ;; repeatedly to get the rest of the buttons. The button states are always in
+  ;; the last bit.
+  ;;
+  ;; In this subroutine, we want to get all button states into a single byte
+  ;; for easy access elsewhere.
+  ;;
+  ;; Based on: http://nintendoage.com/forum/messageview.cfm?catid=22&threadid=8747
+  ;; But with my own tweaks.
+
+	lda #$01
+	sta $4016
+	lda #$00
+	sta $4016
+  sta gamepad_tmp  ; use temp variable to avoid NMI-related concurrency issues
+
+	ldx #$08
+	:
+    lda $4016
+    lsr
+    rol gamepad_tmp
+    dex
+    bne :-
+  lda gamepad_tmp
+  sta gamepad
+	rts
+
+read_gamepad_alt:
+  ;; An alternate method of reading gamepad for reference.
+  ;; From http://forums.nesdev.com/viewtopic.php?t=11151
+
+	lda #$01
+	sta $4016
+	lda #$00
+	sta $4016
+	ldx #$08
+	:
+		pha               ; Push A to stack
+		lda $4016         ; Read next button
+		and #%00000001    ; Rainwarrior's example uses #%11, not #%01 - why?
+		cmp #%00000001    ; If A equals this (or is greater), this sets carry bit.
+		pla               ; Pull top byte in stack into A.
+		rol               ; Rotate left puts carry into rightmost bit.
+		dex
+		bne :-            ; Repeat.
+	sta gamepad         ; We now have the controller state in a single byte.
+	rts
+
 nmi:
+  ;; NMI routine. Transfer sprite to PPU and then make updates based on gamepad
+  ;; state.
+
   lda #<oam
-  sta $2003      ; OAM address
+  sta $2003      ; OAM address. Low byte of address goes here.
   lda #>oam
-  sta $4014      ; OAM DMA
+  sta $4014      ; OAM DMA. High byte goes here, and this starts DMA.
+
+  lda gamepad
+  and #PAD_R
+  beq :+
+    inc oam + 3
+  :
+  lda gamepad
+  and #PAD_L
+  beq :+
+    dec oam + 3
+  :
+  lda gamepad
+  and #PAD_U
+  beq :+
+    dec oam
+  :
+  lda gamepad
+  and #PAD_D
+  beq :+
+    inc oam
+  :
+  lda gamepad
+  and #PAD_B
+  beq :+
+    dec oam + 3
+    dec oam + 3
+  :
+  lda gamepad
+  and #PAD_A
+  beq :+
+    inc oam + 3
+    inc oam + 3
+  :
+  lda gamepad
+  and #PAD_START
+  beq :+
+    inc oam + 2
+  :
+  lda gamepad
+  and #PAD_SELECT
+  beq :+
+    inc oam + 2
+  :
+
   rti
 
 
@@ -144,6 +260,7 @@ reset:
   sta $2001       ; PPU mask
 
   :
+    jsr read_gamepad
     jmp :-
 
 
@@ -155,7 +272,7 @@ irq:
 
 palettes:
   .byte $0f,$31,$32,$33,$0f,$35,$36,$37,$0f,$39,$3a,$3b,$0f,$3d,$3e,$0f
-  .byte $0f,$2b,$11,$16,$0f,$02,$38,$3c,$0f,$1c,$15,$14,$0f,$02,$38,$3c
+  .byte $03,$1c,$2b,$39,$0f,$02,$38,$3c,$0f,$1c,$15,$14,$0f,$02,$38,$3c
 
 sprites:
   .byte $80,$00,$00,$80
